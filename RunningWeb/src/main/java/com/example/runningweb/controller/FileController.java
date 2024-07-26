867f8a1bde854dd6327bab55b09aeb34bb7e2ef8
HeoManBo <iovesssr__@naver.com>
2024-07-26 23:55:16 +0900

    [Feat] 파일 다운로드 기능 추가 및 파일 이미지 뷰잉 기능 추가

--- RunningWeb/src/main/java/com/example/runningweb/controller/FileController.java
+++ RunningWeb/src/main/java/com/example/runningweb/controller/FileController.java
@@ -1,23 +1,28 @@
 package com.example.runningweb.controller;
 
 
-import com.example.runningweb.repository.FileRepository;
+import com.example.runningweb.exception.BadRequestException;
 import com.example.runningweb.service.FileService;
 import com.example.runningweb.util.FileUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.*;
 
 @Controller
 @RequiredArgsConstructor
 public class FileController {
 
     private final FileService fileService;
+    private final FileUtil fileUtil;
 
 
     //비동기로 처리
@@ -25,16 +30,36 @@ public class FileController {
     @ResponseBody
     @PreAuthorize("isAuthenticated()") //로그인 한 멤버만 가능하게
     public ResponseEntity<String> deleteFile(@PathVariable("serverFileName")
-                                           String serverFileName){
+                                             String serverFileName) {
 
-        try{
+        try {
             fileService.deleteFile(serverFileName);
-        }catch (RuntimeException ex){
+        } catch (RuntimeException ex) {
             return ResponseEntity.badRequest()
                     .body(ex.getMessage());
         }
 
         return ResponseEntity.ok().body("삭제되었습니다.");
+    }
+
+    @GetMapping("/file/{serverFileName}")
+    public void fileDownload(@PathVariable("serverFileName") String serverFileName,
+                             HttpServletResponse response) throws IOException {
+
+        File downloadFile = fileUtil.loadFile(serverFileName);
+        if (downloadFile == null) {
+            throw new BadRequestException("파일이 존재하지 않습니다.");
+        }
+
+        response.setContentType("application/download");
+        response.setContentLength((int) downloadFile.length());
+        response.setHeader("Content-disposition", "attachment;filename=\"" + downloadFile + "\"");
+
+        FileInputStream fis = new FileInputStream(downloadFile);
+        OutputStream os = response.getOutputStream();
+        FileCopyUtils.copy(fis, os);
+        fis.close();
+        os.close();
     }
 
 }
